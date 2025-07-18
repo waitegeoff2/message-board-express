5c2eae5f0ed9652c7642c7850da5780ceb242096
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-07-18 14:10:22 -0400

    Added an index EJS file to display dynamic messages on the page.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,26 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Hello dude",
+        user: "Geoff",
+        added: new Date()
+    },
+    {
+        text: "Hello man",
+        user: "Jim",
+        added: new Date()
+    },
+    {
+        text: "Hello there",
+        user: "Jon",
+        added: new Date()
+    },
+]
+
 indexRouter.get('/', (req, res) => {
-    res.send("home page")
+    res.render("index", { title: "Mini Messageboard", messages: messages })
 })
 
 indexRouter.get('/new', (req, res) => {
