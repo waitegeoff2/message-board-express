35eed7dc1774753bb213d0cf434dde4a69a93f8a
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-07-18 12:55:15 -0400

    Added a new route for /new in indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,6 +5,10 @@ indexRouter.get('/', (req, res) => {
     res.send("home page")
 })
 
+indexRouter.get('/new', (req, res) => {
+    res.send("new message")
+})
+
 module.exports = indexRouter;
 
 // add routes for / and /new
